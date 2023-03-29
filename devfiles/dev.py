210fdb0beb897ea4f823830472edafe6e67e6b97
theodoreaouad <theodoreaouad@gmail.com>
2023-03-29 16:03:30 +0200

    thinking on projection

--- devfiles/dev.py
+++ devfiles/dev.py
@@ -1,46 +1,21 @@
-import deep_morpho.initializer as inits
-import deep_morpho.models.bise_base as bise_base
-import deep_morpho.models.lightning_bimonn as lbimonn
-import deep_morpho.binarization.bise_closest_selem as bcs
-
-import torch.nn as nn
-import torch.optim as optim
-
-n_classes = 10
-
-
-model_args = {
-    "kernel_size": (5, 5),
-    "channels": [100],
-    "atomic_element": 'bisel',
-    "input_size": (1, 28, 28),
-    "n_classes": n_classes,
-    "threshold_mode": {
-        "weight": 'softplus',
-        "activation": 'tanh',
-    },
-    "initializer_method": inits.InitBimonnEnum.INPUT_MEAN,
-    "initializer_args": {
-        "bise_init_method": inits.InitBiseEnum.CUSTOM_CONSTANT_RANDOM_BIAS,
-        "lui_init_method": inits.InitBiseEnum.CUSTOM_CONSTANT_CONSTANT_WEIGHTS_RANDOM_BIAS,
-        "bise_init_args": {"ub": 1e-2, "max_output_value": 0.95, "p_for_init": "auto"},
-        "input_mean": .5,
-    },
-    "closest_selem_method": bise_base.ClosestSelemEnum.MIN_DIST_DIST_TO_CST,
-    "bias_optim_mode": bise_base.BiseBiasOptimEnum.POSITIVE,
-    "bias_optim_args": {"offset": 0},
-    "weights_optim_mode": bise_base.BiseWeightsOptimEnum.THRESHOLDED,
-    "weights_optim_args": {"constant_P": True, "factor": 1},
-}
-
-
-model = lbimonn.LightningBiMoNNClassifierLastLinearNotBinary(
-    learning_rate=1e-1,
-    loss=nn.BCELoss(),
-    optimizer=optim.Adam,
-    model_args=model_args,
-)
-bise_net = model.model.layers[-1].bises
-bise_net.find_selem_and_operation()
-bise_net.closest_selem_handler = bcs.BiseClosestMinDistOnCst2(bise_module=bise_net)
-bise_net.binary(update_binaries=True)
+$$\begin{align}
+A &\defeq \begin{pmatrix}
+I_{c} & \mathbf1_{c} & \mathbf0_{c, a } & \mathbf0_{c}\\
+\mathbf0_{a, c} & \mathbf0_{a} & -I_{a} & \mathbf1_{a}\\
+\mathbf0_{c}^T & -1 & \mathbf1_{a} & -1\\
+\mathbf{1}_{c} & 0 & \mathbf0_{a} & -1
+\end{pmatrix} \in \R^{(c + a + 2) \times (c + a + 2)}\\
+X &\defeq  \begin{pmatrix}
+(w_i)_{i \in \Omega\setminus S}\\
+\lambda_0\\
+(\lambda_t)_{t \in T}\\
+b
+\end{pmatrix} \in \R^{c + a + 2}\\
+Y &\defeq \begin{pmatrix}
+(\hat w_i)_{i \in \Omega\setminus S}\\
+(\hat w_t)_{t \in T}\\
+\hat b\\
+0
+\end{pmatrix} \in \R^{c + a + 2}
+\end{align}
+$$
