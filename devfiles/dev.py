f29013651c4e06e98fa5374026af4d6090a5b589
theodoreaouad <theodoreaouad@gmail.com>
2023-04-04 00:24:09 +0200

    add thinking on projection

--- devfiles/dev.py
+++ devfiles/dev.py
@@ -1,21 +1,9 @@
-$$\begin{align}
-A &\defeq \begin{pmatrix}
-I_{c} & \mathbf1_{c} & \mathbf0_{c, a } & \mathbf0_{c}\\
-\mathbf0_{a, c} & \mathbf0_{a} & -I_{a} & \mathbf1_{a}\\
-\mathbf0_{c}^T & -1 & \mathbf1_{a} & -1\\
-\mathbf{1}_{c} & 0 & \mathbf0_{a} & -1
-\end{pmatrix} \in \R^{(c + a + 2) \times (c + a + 2)}\\
-X &\defeq  \begin{pmatrix}
-(w_i)_{i \in \Omega\setminus S}\\
-\lambda_0\\
-(\lambda_t)_{t \in T}\\
-b
-\end{pmatrix} \in \R^{c + a + 2}\\
-Y &\defeq \begin{pmatrix}
-(\hat w_i)_{i \in \Omega\setminus S}\\
-(\hat w_t)_{t \in T}\\
-\hat b\\
-0
-\end{pmatrix} \in \R^{c + a + 2}
-\end{align}
-$$
+a = 1
+b = 1
+c = 1
+h = 1
+f = 1
+g = 1
+
+((a + 1) / ((1 + b * (a + 1))^2)) * (b^2 * (c + g)^2 + h^2 + 2 * b * (c + g) * h) - (2 / (1 + b * (a + 1))) * (b * (c + g) + h) * (c + g) + f + g^2 + (b / (1 + b * (a + 1))) * ((a - 1) * h - c - g)
+((a + 1) / (d^2)) * (b^2 * (c + g)^2 + h^2 + 2 * b * (c + g) * h) - (2 / d) * (b * (c + g) + h) * (c + g) + f + g^2 + (b / d) * ((a - 1) * h - c - g)
